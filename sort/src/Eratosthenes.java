5149db502e5aa7d9eaeb51e3e2664631bcbd4392
WangDuDu <496977440@qq.com>
2018-04-06 12:30:23 +0800

    添加埃拉托斯特尼筛法的注释

--- sort/src/Eratosthenes.java
+++ sort/src/Eratosthenes.java
@@ -1,5 +1,8 @@
 /**
  * Created by wangshuyang on 2018-4-6.
+ * 埃拉托斯特尼筛法
+ * input 输入的是一个max代表要求出从0到max的所有素数
+ * ouput 输出的是0到max的所有素数
  */
 public class Eratosthenes {
     public static boolean[] sieveOfEratosthenes (int max) {
@@ -23,18 +26,29 @@ public class Eratosthenes {
 
     }
 
+    /*
+     * 初始化boolean数组,除了0,1之外全部初始化为false
+     */
     public static void init (boolean[] flags) {
         for (int i = 2; i < flags.length; i++) {
             flags[i] = true;
         }
     }
 
+    /*
+     * 划掉所有可能被prime整除的数,要保留prime本身,
+     * 所以遍历从prime*prime开始,因为如果如果k*prime且k<prime
+     * 这个值早就在之前的迭代中被划掉了
+     */
     public static void crossOff (boolean[] flags, int prime) {
         for (int i = prime * prime; i < flags.length; i += prime) {
             flags[i] = false;
         }
     }
 
+    /*
+    找出下一个素数
+     */
     public static int getNextPrime (boolean[] flags, int prime) {
         int next = prime + 1;
         while (next < flags.length && flags[next] == false) {
@@ -44,6 +58,9 @@ public class Eratosthenes {
         return next;
     }
 
+    /*
+     * 打印出最后的结果
+     */
     public static void print (boolean[] flags) {
         for (int i = 0; i < flags.length; i++) {
             if (flags[i] == true) {
